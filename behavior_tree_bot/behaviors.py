bd186708522adb357d92b835b1a25889870faf8f
Edmond Dooley <etdooley@ucsc.edu>
2016-10-24 18:34:59 -0700

    Changes to behaviors and checks.

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -11,17 +11,31 @@ def start_timer(state, data, parameters):
 
     
 def initialize_ships_and_deployments(state, data, parameters):
-    data["num_available_ships"] = 0
+    # initialize data elements    
     data["available_ships"] = {}
+    data["deployments"] = PriorityQueue()
+    
+    # find available ships
     for planet in state.my_planets():
-        data["num_available_ships"] += planet.num_ships - 1
         data["available_ships"][planet] = planet.num_ships - 1
-    data["deployments"] = []
     return True
 
 
 def find_focus_point(state, data, parameters):
+    data["focus_x"] = 0
+    data["focus_y"] = 0  
+    for planet in state.my_planets():
+        data["focus_x"] += planet.x
+        data["focus_y"] += planet.y
+    data["focus_x"] /= len(state.my_planets())
+    data["focus_y"] /= len(state.my_planets())
     return True
+
+
+def assess_defense(state, data, parameters):
+    for enemy_fleet in state.enemy_fleets():
+        if enemy_fleet.destination_planet.owner == 1:
+            pass
 
 
 def deploy_fleet(state, data, parameters):
@@ -33,12 +47,16 @@ def deploy_fleet(state, data, parameters):
     ships = data["available_ships"]
     deployments = data["deployments"]
     
+    # all deployments processed, return false
     if not deployments:
         return False
-        
+    
+    # get next deployment
     score, target, num_ships = deployments.get()
     
+    # loop until ship requirement met
     while num_ships > 0 and ships:
+        # find closest planet with available ships
         closest_planet = None
         closest_planet_distance = inf
         for planet in ships.keys():
@@ -47,12 +65,15 @@ def deploy_fleet(state, data, parameters):
                 closest_planet = planet
                 closest_planet_distance = planet_distance
         
-        fleet_size = min(planet.num_ships, num_ships)
+        # send ships
+        fleet_size = min(planet.num_ships - 1, num_ships)
         issue_order(state, closest_planet, target, fleet_size)
         
+        # reduce number of available ships
         num_ships -= fleet_size
         ships[closest_planet] -= fleet_size
         if ships[closest_planet] <= 0:
             del ships[closest_planet]
-        
+    
+    # return true if ships still available
     return ships
--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -2,7 +2,7 @@ from timeit import default_timer as time
 
 
 def check_ships_available(state, data, parameters):
-    return data["num_available_ships"] > 0
+    return data["available_ships"]
 
 
 def check_time_remaining(state, data, parameters):
